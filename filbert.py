b123fde4457fa6f621b9d976dda4e414be3068a2
FilbertR <filbert.reimmer@ashesi.edu.gh>
2025-09-17 00:48:39 +0100

    Create filbert.py

---
+++ filbert.py
@@ -0,0 +1,5 @@
+def filbert():
+    print("Hello, I am Filbert.")
+    print("I like football, my favourite player is Eden Hazard.")
+    print("Eden Hazard is a retired footballer from Belgium.")
+
